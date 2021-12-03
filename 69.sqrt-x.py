48d7e2e5853ddbd1b4baf6d83b8beb4400916d8c
Shubham Dohare <sdohare11@gmail.com>
2021-12-03 18:33:05 +0530

    Done: 69.sqrt-x

---
+++ 69.sqrt-x.py
@@ -0,0 +1,15 @@
+from typing import *
+
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+import math as m
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(m.sqrt(x))
+# @lc code=end
+
