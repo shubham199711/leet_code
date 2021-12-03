cce5fa815a26303fd2fac5298674b3b9cb2d86f1
Shubham Dohare <sdohare11@gmail.com>
2021-12-03 18:48:07 +0530

    Minor fix: 70.climbing-stairs

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -10,10 +10,10 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 3:
             return n
-        a , b = 1 , 2
-        for i in range(2, n):
+        a, b = 1 , 2
+        for _ in range(2, n):
             current = a + b
-            a , b = b , current
+            a, b = b, current
         return b
 # @lc code=end
 
