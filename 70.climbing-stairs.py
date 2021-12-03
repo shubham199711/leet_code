d2af7a0ed28410d6bfb2db0e51de80836c02ebd5
Shubham Dohare <sdohare11@gmail.com>
2021-12-03 18:46:49 +0530

    Done: 70.climbing-stairs

---
+++ 70.climbing-stairs.py
@@ -0,0 +1,19 @@
+from typing import *
+#
+# @lc app=leetcode id=70 lang=python3
+#
+# [70] Climbing Stairs
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3:
+            return n
+        a , b = 1 , 2
+        for i in range(2, n):
+            current = a + b
+            a , b = b , current
+        return b
+# @lc code=end
+
