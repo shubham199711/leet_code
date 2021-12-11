a4d5bad243954e9546994a849c4d386c2a73cb93
Shubham Dohare <sdohare11@gmail.com>
2021-12-11 18:02:49 +0530

    Done: 168.excel-sheet-column-title

---
+++ 168.excel-sheet-column-title.py
@@ -0,0 +1,25 @@
+from types import resolve_bases
+from typing import *
+#
+# @lc app=leetcode id=168 lang=python3
+#
+# [168] Excel Sheet Column Title
+#
+
+# @lc code=start
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        d='0ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        res = ''
+        if columnNumber < 27:
+            return d[columnNumber]
+        while columnNumber > 0:
+            columnNumber,r= divmod(columnNumber,26)
+            if r == 0:
+                columnNumber -= 1
+                r += 26
+            res = d[r] + res
+        return res
+# @lc code=end
+
+
