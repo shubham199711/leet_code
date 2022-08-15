eb6090b18d4ecbd524f7572070bc41c8a4300f3a
Shubham Dohare <sdohare11@gmail.com>
2022-08-15 11:19:43 +0530

    looking into requirements.txt

--- 17.letter-combinations-of-a-phone-number.py
+++ 17.letter-combinations-of-a-phone-number.py
@@ -6,17 +6,17 @@
 
 # @lc code=start
 digit_char_map = {
-    '2': ['a','b','c']
-    '3': ['d','e','f']
-    '4': ['g','h','i']
-    '5': ['j','k','l']
-    '6': ['m','n','o']
-    '7': ['p','q','r','s']
-    '8': ['t','u','v']
+    '2': ['a','b','c'],
+    '3': ['d','e','f'],
+    '4': ['g','h','i'],
+    '5': ['j','k','l'],
+    '6': ['m','n','o'],
+    '7': ['p','q','r','s'],
+    '8': ['t','u','v'],
     '9': ['w','x','y','z']
 }
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        
+    def letterCombinations(self, digits: str):
+        pass
 # @lc code=end
 
