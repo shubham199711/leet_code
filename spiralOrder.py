a156538b3cd52df3960385db4948fb2e22bebcdd
Shubham Dohare <sdohare11@gmail.com>
2022-08-21 20:22:30 +0530

    Added: spiralOrder to iterative

--- spiralOrder.py
+++ spiralOrder.py
@@ -1,3 +1,9 @@
 class Solution:
+    # def spiralOrder(self, matrix):
+    #     return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
     def spiralOrder(self, matrix):
-        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+        _ans = []
+        while matrix:
+            _ans.extend([*matrix.pop(0)])
+            matrix = [*zip(*matrix)][::-1]
+        return _ans
