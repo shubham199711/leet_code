e834811e9f4ec500fa7bd5f3b00e836aeb76a37d
Shubham Dohare <sdohare11@gmail.com>
2022-08-23 08:50:16 +0530

    Added: copyRandomList

---
+++ copyRandomList.py
@@ -0,0 +1,16 @@
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if head is None: return None
+        mapping = {}
+        cur = head
+        while cur:
+            mapping[cur] = Node(cur.val,None,None)
+            cur = cur.next
+        cur = head
+        while cur:
+            if cur.next:
+                mapping[cur].next = mapping[cur.next]
+            if cur.random:
+                mapping[cur].random = mapping[cur.random]
+            cur = cur.next
+        return mapping[head]
