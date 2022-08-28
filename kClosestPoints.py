076e74bde2977de2fefd0bda765c94dcfb5c36ba
Shubham Dohare <sdohare11@gmail.com>
2022-08-28 18:57:29 +0530

    Modified:  -> kClosestPoints.py

--- kClosestPoints.py
+++ kClosestPoints.py
@@ -1,8 +1,15 @@
 import bisect
-class Solution:
+class Solution2:
     def kClosest(self, points, k):
         _distanceList = []
         for i,item in enumerate(points):
             distance = item[0] * item[0] + item[1] * item[1]
             bisect.insort(_distanceList, [distance, i], key = lambda x: x[0])  # type: ignore
         return [points[_distanceList[x][1]] for x in range(0, k)]
+
+
+# faster
+import heapq
+class Solution:
+    def kClosest(self, points, k: int):
+        return heapq.nsmallest(k, points, lambda p: p[0] * p[0] + p[1] * p[1])
