0ce5d8390ad39237ae2e40db461df1dbc6263f67
Shubham Dohare <sdohare11@gmail.com>
2022-09-03 22:17:28 +0530

    Added: findMinRotatedArray.py

---
+++ findMinRotatedArray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1 or nums[0] < nums[-1]:
+            return nums[0]
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid > 0 and nums[mid] < nums[mid - 1]:
+                return nums[mid]
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
