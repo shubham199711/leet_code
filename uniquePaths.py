47de50f14e4bff2c0b05321b48a5fb28696e780a
Shubham Dohare <sdohare11@gmail.com>
2022-09-03 10:47:21 +0530

    Added: uniquePaths.py

---
+++ uniquePaths.py
@@ -0,0 +1,12 @@
+from math import factorial
+
+class Solution:
+    def uniquePaths2(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[j] = dp[j -1] + dp[j]
+        return dp[-1] if m and n else 0
+    
+    def uniquePaths(self, m: int, n: int) -> int:
+        return factorial(m+n -2) // factorial(m -1) // factorial(n -1)
