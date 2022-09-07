c891ad699abbbc517c0d1de328f614d0d1976565
Shubham Dohare <sdohare11@gmail.com>
2022-09-07 14:18:22 +0530

    Added: lowestCommonAncestor

--- lowestCommonAncestor.py
+++ lowestCommonAncestor.py
@@ -5,6 +5,12 @@
 #         self.left = None
 #         self.right = None
 
+# Let large = max(p.val, q.val), small = min(p.val, q.val).
+# We keep iterate root in our BST:
+# If root.val > large then both node p and q belong to the left subtree, go to left by root = root.left.
+# If root.val < small then both node p and q belong to the right subtree, go to right by root = root.right.
+# Now, small <= root.val <= large the current root is the LCA between q and p.
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         _max = max(p.val, q.val)
