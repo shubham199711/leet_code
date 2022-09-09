0f87a42c7ba18ed08561f0a345fa9ea356ca5b1f
Shubham Dohare <sdohare11@gmail.com>
2022-09-09 20:05:20 +0530

    Fixed: 21.merge-two-sorted-lists.py

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -1,42 +1,25 @@
-#
-# @lc app=leetcode id=21 lang=python3
-#
-# [21] Merge Two Sorted Lists
-#
-
-# @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 
-# 1 2 4
-# 1 3 4
-# 1(b) 1(a) 2  
+from queue import PriorityQueue
 class Solution:
-    def mergeTwoLists(self, p, q):
-        if p is None:
-            return q
-        head = p
-        prev = None
-        while p is not None:
-                while q is not None and q.val <= p.val:
-                    q_temp = q
-                    q = q.next
-                    q_temp.next = p
-                    if p == head:
-                        head = q_temp
-                    if prev is not None:
-                        prev.next = q_temp
-                    prev = q_temp
-                prev = p
-                p = p.next
-        if q is not None:
-            p = head
-            while p.next is not None:
-                p = p.next
-            p.next = q
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        prorityQueue = PriorityQueue()
+        _lists = [list1, list2]
+        for _list in _lists:
+            while _list is not None:
+                prorityQueue.put((_list.val,_list.val))
+                _list = _list.next
+        head = running = None
+        while not prorityQueue.empty():
+            item = prorityQueue.get()[1]
+            if head is None:
+                head = running = ListNode(item)
+            else:
+                temp = ListNode(item)
+                running.next = temp
+                running = running.next
         return head
-# @lc code=end
-
