46c56b883d15608ed3b1ee138877c9f33dbd532e
Shubham Dohare <sdohare11@gmail.com>
2022-09-12 09:40:44 +0530

    Modified: threeSum.py for k value

--- threeSum.py
+++ threeSum.py
@@ -1,17 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         _ans = {}
+        k = 0
         nums.sort()
         for i, item in enumerate(nums):
             li, hi = i + 1, len(nums) - 1
             while li < hi:
                 _sum = item + nums[li] + nums[hi]
-                if _sum == 0:
+                if _sum == k:
                     key = (item, nums[li], nums[hi])
                     _ans[key] = True
                     li += 1
                     hi -= 1
-                elif _sum < 0:
+                elif _sum < k:
                     li += 1
                 else:
                     hi -= 1
