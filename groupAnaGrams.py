05f61404637cbb13cdd64c0dc188e74cd98d5417
Shubham Dohare <sdohare11@gmail.com>
2022-09-19 14:08:06 +0530

    Improved: groupAnaGrams.py

--- groupAnaGrams.py
+++ groupAnaGrams.py
@@ -4,12 +4,7 @@ class Solution:
         for item in arr:
             sortedItem = ''.join(sorted(item))
             if m.get(sortedItem) is not None:
-                temp = m[sortedItem]
-                temp.append(item)
-                m[sortedItem] = temp
+                m[sortedItem].append(item)
             else:
                 m[sortedItem] = [item]
-        _ans = []
-        for key in m:
-            _ans.append(m[key])
-        return _ans
+        return [m[key] for key in m.keys()] 
