f175ec0dbfd29083bb3da26187ab67916b3bb1f3
Shubham Dohare <sdohare11@gmail.com>
2022-10-05 19:20:39 +0530

    Time: 67 ms (20.88%), Space: 13.8 MB (58.69%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,8 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        return map(int ,str(int(''.join(map(str, digits))) + 1))
+    
+            
+        
+        
+        
