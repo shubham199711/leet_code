8be706fa48d32c7515c28f5edd13253263eb3194
Shubham Dohare <sdohare11@gmail.com>
2022-11-20 14:50:38 +0530

    Time: 1657 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 
--- select id from Weather a where a.temperature > (select temperature from Weather b where a.recordDate = subdate(b.recordDate, 1))
+select id from Weather a where a.temperature > (select temperature from Weather b where subdate(a.recordDate, 1) = b.recordDate)
 
-select a.id from Weather a, Weather b where DATEDIFF(a.recordDate, b.recordDate) = 1 and a.temperature > b.temperature;
+-- select a.id from Weather a, Weather b where DATEDIFF(a.recordDate, b.recordDate) = 1 and a.temperature > b.temperature;
