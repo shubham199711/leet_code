c620863afcd64293127b1a292ce589920901f04e
Shubham Dohare <sdohare11@gmail.com>
2023-01-16 19:07:11 +0530

    Time: 38 ms (57.14%), Space: 13.9 MB (27.46%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def letterCombinations(self, digits):
+    def letterCombinations_old(self, digits):
         if not digits: 
             return []
         char_map = { 
@@ -15,3 +15,49 @@ class Solution(object):
         chars = [char_map[d] for d in digits]
         l = list(itertools.product(*chars))
         return [''.join(i) for i in l]
+
+    def letterCombinationsWithRecursion(self, digits):
+        if not digits: 
+            return []
+        char_map = { 
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        ans  = []
+        def backtrack(i, currentStr):
+            if len(currentStr) == len(digits):
+                ans.append(currentStr)
+                return
+            for item in char_map[digits[i]]:
+                backtrack(i + 1, currentStr + item)
+        backtrack(0, "")
+        return ans
+
+    def letterCombinations(self, digits):
+        if not digits: 
+            return []
+        char_map = { 
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        queue  = [""]
+        for item in digits:
+            item = char_map[item]
+            ansAns = []
+            for innerItem in queue:
+                for j in item:
+                    ansAns.append(innerItem + j)
+            queue = ansAns
+        return queue
