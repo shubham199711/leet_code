9672855a282d92ee28b1efeb7f1f0688797c4ed6
Shubham Dohare <sdohare11@gmail.com>
2023-01-21 19:35:07 +0530

    Time: 27 ms (94.54%), Space: 14 MB (25.41%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+import bisect
+class Solution:
+    # with sorted array
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        sortedAns = []
+        sumAns = []
+        def btsInPleace(head):
+            if not head:
+                return
+            bisect.insort(sortedAns, head.val)
+            btsInPleace(head.left)
+            btsInPleace(head.right)
+        def sumArray():
+            runningSum = 0
+            for i, item in enumerate(sortedAns[::-1]):
+                runningSum += item
+                sumAns.append(runningSum)
+                
+        def btsInPleaceAndUpdate(head):
+            if not head:
+                return
+            index = bisect.bisect(sortedAns, head.val)
+            head.val = sumAns[-index]
+            btsInPleaceAndUpdate(head.left)
+            btsInPleaceAndUpdate(head.right)
+        
+        btsInPleace(root)
+        sumArray()
+        print(sortedAns)
+        print(sumAns)
+        btsInPleaceAndUpdate(root)
+        return root
+                
+            
+            
+        
