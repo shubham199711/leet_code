b247251b80523abe256d53c16bea62ac7e98207c
Shubham Dohare <sdohare11@gmail.com>
2023-01-30 18:06:01 +0530

    Time: 1527 ms (49.52%), Space: 31.6 MB (33.91%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,6 +1,8 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        return sorted(nums, key=lambda x: x-pivot and (1, -1)[x<pivot])
+        # [1, -1][x < pivot] is a trick to keep the order of array
+        return sorted(nums, key=lambda x: x-pivot and [1, -1][x < pivot])
+        # manual solution
         lessThenPivot = []
         moreThenPivot = []
         equalThenPivot = []
