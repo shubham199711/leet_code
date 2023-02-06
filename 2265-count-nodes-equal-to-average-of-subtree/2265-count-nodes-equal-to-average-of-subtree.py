7f1996b9da199862563c0730f90c35b07dc1dfb7
Shubham Dohare <sdohare11@gmail.com>
2023-02-06 19:00:39 +0530

    Time: 46 ms (79.10%), Space: 15.2 MB (27.98%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -6,38 +6,18 @@
 #         self.right = right
 class Solution:
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
-        _ans = 0
-        runningSum = 0
-        runningCount = 0
-        cache = {}
-        def sumAndCount(_head):
-            nonlocal runningSum, runningCount, cache
-            if _head is None:
-                return 0, 1
-            if cache.get(id(_head)) is not None:
-                runningSum += cache[id(_head)][0]
-                runningCount += cache[id(_head)][1]
-                return runningSum, runningCount
-            runningSum += _head.val
-            runningCount += 1
-            sumAndCount(_head.left)
-            sumAndCount(_head.right)
-            cache[id(_head)] = runningSum, runningCount
-            return runningSum, runningCount
-            
-        
-        def dfs(head):
-            nonlocal _ans, runningSum, runningCount, cache
-            if head is None:
-                return 
-            dfs(head.left)
-            dfs(head.right)
-            runningSum, runningCount = 0 , 0
-            currentVal,currentCount = sumAndCount(head)
-            if (currentVal // currentCount) == head.val:
-                _ans += 1
-            # print(f'{head.val = }, {currentVal = }, {currentCount = }, {_ans = }, {cache = }')
-                
+        self.count=0
+        def dfs(node):
+            if(not node):
+                return [0,0]
+            left=dfs(node.left)
+            right=dfs(node.right)
+            left_sum,left_count=left[0],left[1]
+            right_sum,right_count=right[0],right[1]
+            tot=node.val+left_sum+right_sum
+            if(node.val==tot//(1+left_count+right_count)):
+                self.count+=1
+            return [tot,1+left_count+right_count]
         dfs(root)
-        return _ans
+        return self.count
         
