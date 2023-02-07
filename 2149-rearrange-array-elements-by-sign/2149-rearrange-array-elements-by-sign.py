aaed7e2b9fe46c0f5e57e046a2856b0187251e8a
Shubham Dohare <sdohare11@gmail.com>
2023-02-07 18:44:18 +0530

    Time: 1580 ms (75.92%), Space: 45.4 MB (37.96%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,15 +1,28 @@
+# class Solution:
+#     def rearrangeArray(self, nums: List[int]) -> List[int]:
+#         positiveStack = []
+#         negetiveStack = []
+#         ans = []
+#         for item in nums:
+#             if item < 0:
+#                 negetiveStack.append(item)
+#             else:
+#                 positiveStack.append(item)
+#         for i in range(len(positiveStack)):
+#             ans.append(positiveStack[i])
+#             ans.append(negetiveStack[i])
+#         return ans
+        
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        positiveStack = []
-        negetiveStack = []
-        ans = []
-        for item in nums:
-            if item < 0:
-                negetiveStack.append(item)
+        a=[0]*len(nums)
+        p=0
+        n=1
+        for i in nums:
+            if i>0:
+                a[p]=i
+                p+=2
             else:
-                positiveStack.append(item)
-        for i in range(len(positiveStack)):
-            ans.append(positiveStack[i])
-            ans.append(negetiveStack[i])
-        return ans
-        
+                a[n]=i
+                n+=2
+        return a
