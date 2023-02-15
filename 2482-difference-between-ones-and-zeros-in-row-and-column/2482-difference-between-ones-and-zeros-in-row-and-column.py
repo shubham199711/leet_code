0c930d55ce711824512694cbe77a72934cc9937e
Shubham Dohare <sdohare11@gmail.com>
2023-02-15 22:40:15 +0530

    Time: 1786 ms (69.45%), Space: 49.4 MB (55.24%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,12 +1,14 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        rowLen = len(grid)
+        colLen = len(grid[0])
         rowCount0 = [x.count(0) for x in grid]
-        rowCount1 = [x.count(1) for x in grid]
+        rowCount1 = [rowLen - x for x in rowCount0]
         colCount0 = [x.count(0) for x in zip(*grid)]
-        colCount1 = [x.count(1) for x in zip(*grid)]
+        colCount1 = [colLen - x for x in colCount0]
         ans = []
-        for row in range(len(grid)):
-            _ans = [rowCount1[row] + colCount1[col] - rowCount0[row] - colCount0[col] for col in range(len(grid[0]))]
+        for row in range(rowLen):
+            _ans = [rowCount1[row] + colCount1[col] - rowCount0[row] - colCount0[col] for col in range(colLen)]
             ans.append(_ans)
         return ans
         
