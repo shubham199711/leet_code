2f3363ae39a19f518ccdcf8a3bcf8e4972938688
Shubham Dohare <sdohare11@gmail.com>
2023-02-20 18:47:20 +0530

    Time: 25 ms (98.20%), Space: 13.8 MB (97.60%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,6 +1,10 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        winner = 0
-        for i in range(1, n+1):
-            winner = (winner + k) % i
-        return winner + 1
+        players = [x + 1 for x in range(n)]
+        i = 0
+        while len(players) > 1:
+            i = (i + k - 1) % len(players)
+            players.pop(i)
+        return players[0]
+        
+        
