76e0d176ed035e06760738baf839f56a285c5658
Shubham Dohare <sdohare11@gmail.com>
2023-02-21 20:19:17 +0530

    Time: 45 ms (50.46%), Space: 13.9 MB (62.59%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,12 +1,12 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         #return str(int(num1) * int(num2))
-        n1, n2 = int(num1[0]), int(num2[0])
+        n1, n2 = 0, 0
         ord_0 = ord("0")
-        for item in num1[1:]:
+        for item in num1:
             n1 *= 10
             n1 += ord(item)-ord_0
-        for item in num2[1:]:
+        for item in num2:
             n2 *= 10
             n2 += ord(item)-ord_0
         return str(n1 * n2)
