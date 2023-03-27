c90642c93246fecbdf38160d1a88820397634ebb
Shubham Dohare <sdohare11@gmail.com>
2023-03-27 20:24:29 +0530

    Time: 74 ms (73.88%), Space: 17 MB (11.18%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -3,10 +3,10 @@ class Solution:
         @lru_cache(None)
         def dp(x,y):
             if x==m-1 and y==n-1:
-                return max(1,-grid[x][y]+1)
+                return max(1,-grid[x][y]+1)# negetive so that if last value in negetive then that should be picked
             if x>=m or y>=n:
                 return float('inf')
-            return max(1,min(dp(x+1,y),dp(x,y+1))-grid[x][y])
+            return max(1,min(dp(x+1,y),dp(x,y+1))-grid[x][y]) #best next move value minus current or 1
         
         m,n=len(grid),len(grid[0])
         return dp(0,0)
