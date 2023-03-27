d5985eabf5a9b8a3596caf007f57cc26d0a2cd6a
Shubham Dohare <sdohare11@gmail.com>
2023-03-27 20:17:54 +0530

    Time: 85 ms (24.59%), Space: 17.2 MB (5.79%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -1,5 +1,7 @@
 class Solution:
     def calculateMinimumHP(self, grid: List[List[int]]) -> int:
+        m,n=len(grid),len(grid[0])
+        
         @lru_cache(None)
         def dp(x,y):
             if x==m-1 and y==n-1:
@@ -8,5 +10,4 @@ class Solution:
                 return float('inf')
             return max(1,min(dp(x+1,y),dp(x,y+1))-grid[x][y])
         
-        m,n=len(grid),len(grid[0])
         return dp(0,0)
