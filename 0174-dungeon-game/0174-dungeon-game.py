aad925a912fd8fd35ed9307ea83ccf615071deb5
Shubham Dohare <sdohare11@gmail.com>
2023-03-27 20:19:22 +0530

    Time: 87 ms (19.61%), Space: 17.1 MB (8.43%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -1,13 +1,12 @@
 class Solution:
     def calculateMinimumHP(self, grid: List[List[int]]) -> int:
-        m,n=len(grid),len(grid[0])
-        
         @lru_cache(None)
         def dp(x,y):
             if x==m-1 and y==n-1:
-                return max(1,-grid[m-1][n-1]+1)
+                return max(1,-grid[x][y]+1)
             if x>=m or y>=n:
                 return float('inf')
             return max(1,min(dp(x+1,y),dp(x,y+1))-grid[x][y])
         
+        m,n=len(grid),len(grid[0])
         return dp(0,0)
