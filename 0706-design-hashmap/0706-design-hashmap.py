4d879b65a03b0ab6269222260b919c7f387a97b1
Shubham Dohare <sdohare11@gmail.com>
2023-04-07 10:02:43 +0530

    Time: 267 ms (51.90%), Space: 17.2 MB (77.83%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -0,0 +1,41 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.n = 100
+        self.table = []
+        for i in range(self.n):
+            self.table.append([])
+    def find(self,key):
+        hk = key % self.n
+        for i,v in enumerate(self.table[hk]):
+            if v[0]==key:
+                return hk,i
+        return hk,-1
+    def put(self, key: int, value: int) -> None:
+        hk,idx = self.find(key)
+        if idx!=-1:
+            self.table[hk][idx] = (key,value)
+        else:
+            self.table[hk].append((key,value))
+
+    def get(self, key: int) -> int:
+        hk,idx = self.find(key)
+        if idx!=-1:
+            return self.table[hk][idx][1]
+        else:
+            return -1
+                
+                
+    def remove(self, key: int) -> None:
+        hk,idx = self.find(key)
+        if idx!=-1:
+            del self.table[hk][idx]
+
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
