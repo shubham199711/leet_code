3955ebb40c31abb8a9b248fb887f2bae682b3a7e
Shubham Dohare <sdohare11@gmail.com>
2023-04-07 11:05:54 +0530

    Time: 32 ms (78.93%), Space: 13.8 MB (95.37%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        zero, one, two = 0, 0, 0 
+        for item in nums:
+            if item == 0:
+                zero += 1
+            if item == 1:
+                one += 1
+            if item == 2:
+                two += 1
+        for i in range(len(nums)):
+            if zero > 0:
+                nums[i] = 0
+                zero -= 1
+            elif one > 0:
+                nums[i] = 1
+                one -= 1
+            else:
+                nums[i] = 2
+                two -= 1
