e9e7ad00e8179ed2317f0c86fc6d9429fc37659d
Shubham Dohare <sdohare11@gmail.com>
2023-04-08 14:20:31 +0530

    Time: 33 ms (63.51%), Space: 13.9 MB (25.69%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution2:
     def generateMatrix(self, n):
         matrix = [[None]*n for _ in range(n)]
         # r => row
@@ -6,14 +6,30 @@ class Solution:
         rs,re,cs,ce = 0,n-1,0,n-1
         count = 1
         while rs <= re and cs <= ce:
-            for j in range(cs, ce+1): # fill till end
+            for j in range(cs, ce+1): # fill from start till end
                 matrix[rs][j], count = count, count+1
             for i in range(rs+1, re+1): # fill from start + 1 to end
                 matrix[i][ce], count = count, count+1
             for j in range(ce-1, cs-1, -1): # fill from end -1 to end
                 matrix[re][j], count = count, count+1
-            for i in range(re-1,rs,-1): # full from end - 1 to start - 1 
+            for i in range(re-1,rs,-1): # full from end - 1 to start - 1(as 1 is already filled using start)
                 matrix[i][cs], count = count, count+1
             rs, re = rs+1,re-1
             cs,ce = cs+1,ce-1
         return matrix
+
+class Solution:
+    def generateMatrix(self, n):
+        ans = [[0]*n for _ in range(n)]
+        x = y = j = 0
+        dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+        for i in range(1,n**2+1):
+            ans[x][y] = i
+            dx, dy = dirs[j]
+            if 0<=x+dx<=n-1 and 0<=y+dy<=n-1 and ans[x+dx][y+dy]==0:
+                x, y = x+dx, y+dy
+            else:
+                j = (j+1)%4
+                dx, dy = dirs[j]
+                x, y = x+dx, y+dy   
+        return ans
