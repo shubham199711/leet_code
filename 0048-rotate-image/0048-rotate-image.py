bca8b04b91db1d62a189aafc017c0b8f5e48f364
Shubham Dohare <sdohare11@gmail.com>
2023-04-08 10:09:08 +0530

    Time: 35 ms (74.36%), Space: 13.9 MB (61.05%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,23 +1,14 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        matrix[::] = zip(*matrix[::-1])
-        return
+        # matrix[::] = zip(*matrix[::-1])
+        # return
         '''
         We have to rotate first by horizontal axis and then
         transpose matrix to rotate by 90
         '''
-        # rotate
-        # l = 0
-        # r = len(matrix) -1
-        # while l < r:
-            # matrix[l], matrix[r] = matrix[r], matrix[l]
-            # l += 1
-            # r -= 1
-        
         for i in range(len(matrix) // 2):
-            _len = -1 -i
-            matrix[i], matrix[_len]  = matrix[_len], matrix[i]
-        # transpose 
+            matrix[i], matrix[~i]  = matrix[~i], matrix[i]
+        # transpose only the top right part is moved to bottom left part
         for i in range(len(matrix)):
             for j in range(i + 1, len(matrix)):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
