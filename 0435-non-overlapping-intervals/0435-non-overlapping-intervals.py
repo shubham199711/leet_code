8374b3b71426274a40c34fff7576f775c8e37460
Shubham Dohare <sdohare11@gmail.com>
2023-04-09 14:57:57 +0530

    Time: 1391 ms (63.27%), Space: 52.9 MB (18.47%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,13 +3,12 @@ class Solution:
         intervals.sort(key=lambda x: x[0])
         ans = 0
         prev = intervals[0][-1]
-        print(prev)
         for i in range(1, len(intervals)):
             item = intervals[i]
             if item[0] < prev:
                 ans += 1
-                prev = min(prev, item[1])
+                prev = min(prev, item[1]) # keep the one with minimum end values
             else:
-                prev= max(prev, item[1])
+                prev= max(prev, item[1]) # keep going with max end value
         return ans
         
