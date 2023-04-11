b6419644f5929a2353fb45ad5593cb3538da9ac2
Shubham Dohare <sdohare11@gmail.com>
2023-04-11 08:42:48 +0530

    Time: 40 ms (12.74%), Space: 13.8 MB (97.23%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -7,14 +7,12 @@ class Solution:
             if item == '#':
                 if len(_ansForS):
                     _ansForS.pop()
-                    # _ansForS = _ansForS[:-1]
             else:
                 _ansForS.append(item)
         for item in t:
             if item == '#':
                 if len(_ansForT):
                     _ansForT.pop()
-                    # _ansForT = _ansForT[:-1]
             else:
                 _ansForT.append(item)
         return _ansForS == _ansForT
