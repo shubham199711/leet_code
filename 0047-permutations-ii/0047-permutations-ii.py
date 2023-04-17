9ed816c9e166bd9d458417959726a39cf8065475
Shubham Dohare <sdohare11@gmail.com>
2023-04-17 08:54:13 +0530

    Time: 53 ms (93.76%), Space: 14.4 MB (43.76%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,17 +1,17 @@
 class Solution:
-    def permutations(self,cur,idx,ans):
-        if idx==len(cur):
-            ans.append(cur[::])
-        if idx>=len(cur):return 
-        seen = set()
-        for i in range(idx,len(cur)):
-            if cur[i] not in seen:
-                seen.add(cur[i])
-                cur[i],cur[idx]=cur[idx],cur[i]
-                self.permutations(cur,idx+1,ans)
-                cur[i],cur[idx]=cur[idx],cur[i]
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         ans=[]
         nums.sort()
-        self.permutations(nums,0,ans)
+        def permutations(idx):
+            if idx==len(nums):
+                ans.append(nums[::])
+            if idx>=len(nums): return 
+            seen = set()
+            for i in range(idx,len(nums)):
+                if nums[i] not in seen:
+                    seen.add(nums[i])
+                    nums[i],nums[idx]=nums[idx],nums[i]
+                    permutations(idx+1)
+                    nums[i],nums[idx]=nums[idx],nums[i]
+        permutations(0)
         return ans
