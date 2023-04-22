77ab40be784f395390b6ff14ebb38c992a0277bd
Shubham Dohare <sdohare11@gmail.com>
2023-04-22 08:32:09 +0530

    Time: 39 ms (69.29%), Space: 13.9 MB (89.07%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,16 +1,18 @@
 from collections import deque
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        q = deque([s])
+        q = [s]
         seen = set()
-        while q:
-            s = q.popleft()    # popleft() = BFS ; pop() = DFS
-            for word in wordDict:
-                if s.startswith(word):
-                    new_s = s[len(word):]
-                    if new_s == "": 
-                        return True
-                    if new_s not in seen:
-                        q.append(new_s)
-                        seen.add(new_s)
+        while len(q):
+            s = q.pop(0)
+            for item in wordDict:
+                if not s.startswith(item): 
+                    continue
+                _s = s[len(item):]
+                if _s == "": 
+                    return True
+                if _s not in seen:
+                    seen.add(_s)
+                    q.append(_s)
         return False
+        
