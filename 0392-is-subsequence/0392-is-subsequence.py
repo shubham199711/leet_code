147cc33adee12ea52af2e8ba024b163b73f05c59
Shubham Dohare <sdohare11@gmail.com>
2023-04-28 09:36:00 +0530

    Time: 47 ms (5.34%), Space: 16.3 MB (5.34%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,7 +1,11 @@
 class Solution:
     def isSubsequence(self, s, t):
-        remainder_of_t = iter(t)
-        for letter in s:
-            if letter not in remainder_of_t:
-                return False
-        return True
+        dp = [-2]*(len(s)+1)
+        dp[0] =-1
+        for i in range(1,len(dp)):
+            if dp[i-1] == -2: return False
+            for j in range(dp[i-1]+1, len(t)):
+                if t[j] == s[i-1]:
+                    dp[i] = j
+                    break;
+        return True if dp[-1] !=-2 else False
