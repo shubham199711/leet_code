6951fc7bc12aa22e802e88f56be7c0a94dfcb72c
Shubham Dohare <sdohare11@gmail.com>
2023-11-24 00:26:15 +0530

    Time: 66 ms (60.86%), Space: 17.8 MB (6.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         cache = {}
         for i, x in enumerate(nums):
-            y = target - x
             if cache.get(x) is not None:
                 return cache[x], i
+            y = target - x
             cache[y] = i
         return [-1, -1]
