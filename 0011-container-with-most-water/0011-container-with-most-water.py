9b4a131d8f104f55bddd4bd6837863b5355498c5
Shubham Dohare <sdohare11@gmail.com>
2023-11-24 16:59:14 +0530

    Time: 584 ms (81.12%), Space: 29.3 MB (55.00%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,6 +4,9 @@ class Solution:
         ans = 0
         while left < right:
             max_height_on_curent_level = min(height[left], height[right])
+            # (right - left) gives width
+            # max_height_on_curent_level gives min height
+            # so area = width * height
             ans = max(ans, (right - left) * max_height_on_curent_level)
             if height[left] < height[right]:
                 left += 1
