fd81963c1b2e2240eadb79c081543edaa7282c33
Shubham Dohare <sdohare11@gmail.com>
2023-11-26 11:41:40 +0530

    Time: 40 ms (54.12%), Space: 16.4 MB (41.10%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,21 +4,18 @@ class Solution:
             return 0
         if len(s) and s[0] == '0':
             return 0
-        prev1 = 1
-        prev2 = 1
+        val1 = val2 = 1
         ZERO = ord('0')
         for i in range(1, len(s)):
             curr = ord(s[i]) - ZERO
-            combi = ((ord(s[i - 1]) - ZERO)* 10) + curr
+            prev = (ord(s[i - 1]) - ZERO) * 10 + curr
             val = 0
             if curr >= 1:
-                val += prev2
-            if combi >= 10 and combi <= 26:
-                val += prev1
-            prev1 = prev2
-            prev2 = val
-        return prev2
-                
+                val += val2
+            if prev >= 10 and prev <= 26:
+                val += val1
+            val1 = val2
+            val2 = val
+        return val2
                 
             
-        
