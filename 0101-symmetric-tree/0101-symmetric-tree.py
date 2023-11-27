f2027b5c55b7a6aa54372e046b3a090aa7ab19d8
Shubham Dohare <sdohare11@gmail.com>
2023-11-27 23:23:26 +0530

    Time: 51 ms (6.50%), Space: 16.4 MB (53.12%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -5,13 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSame(self, left, right):
-        if not left or not right:
-            return (left is right)
+    def isSame(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
+        if left is not None and right is None:
+            return False
+        if right is not None and left is None:
+            return False
+        if left is None and right is None:
+            return True
         if left.val != right.val:
             return False
         return self.isSame(left.left, right.right) and self.isSame(left.right, right.left)
-    
+        
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         return self.isSame(root.left, root.right)
         
