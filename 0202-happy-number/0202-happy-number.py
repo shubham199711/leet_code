11e6108248fbee7f8cf298d3ad71985db2b773eb
Shubham Dohare <sdohare11@gmail.com>
2023-12-03 11:42:27 +0530

    Time: 41 ms (65.29%), Space: 16.2 MB (46.90%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,12 +1,13 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         seen = set()
-        memo = {}
+        arr = [None for i in range(10)]
         def memo_square(x):
-            if memo.get(x) is not None:
-                return memo[x]
-            memo[x] = x * x
-            return memo[x]
+            index = ord(str(x)) - ord('0')
+            if arr[index] is not None:
+                return arr[index]
+            arr[index] = x * x
+            return arr[index]
         while n not in seen:
             seen.add(n)
             n = sum([memo_square(int(x)) for x in str(n)])
