d812159a0bcb8a5d67ed9a7608c3f0f49228531a
Shubham Dohare <sdohare11@gmail.com>
2023-12-03 11:36:57 +0530

    Time: 47 ms (26.72%), Space: 16.2 MB (77.29%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,20 +1,10 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         seen = set()
-        n = str(n)
-        while True:
-            ans = []
-            for char in n:
-                if char == '0':
-                    pass
-                else:
-                    ans.append(int(char) ** 2)
-            ans = str(sum(ans))
-            if ans in seen:
-                return False
-            if ans == '1':
+        while n not in seen:
+            seen.add(n)
+            n = sum([int(x) * int(x) for x in str(n)])
+            if n == 1:
                 return True
-            seen.add(ans)
-            n = ans
+        return False
             
-        
