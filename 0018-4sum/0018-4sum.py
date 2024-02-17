bafcfac9dc8c0e661a2f21e83799ae0255de5ca5
Shubham Dohare <sdohare11@gmail.com>
2024-02-17 22:18:48 +0530

    Time: 373 ms (71.74%), Space: 16.7 MB (70.05%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -2,29 +2,27 @@ import itertools
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        ans, quad = [], []
+        ans, demp = [], []
         nums.sort()
-        def kSum(k, start, currentTarget):
+        def k_sum(k, start, current_target):
             if k != 2:
                 for i in range(start, len(nums) - k + 1):
-                    if i > start and nums[i] == nums[i -1]:
+                    if i > start and nums[i] == nums[i - 1]:
                         continue
-                    # quad is buffer where we keep 2 element and then we find remaning with 2-sum solution
-                    quad.append(nums[i])
-                    kSum(k -1, i + 1, currentTarget - nums[i])
-                    quad.pop()
-                return
-            # base case
+                    demp.append(nums[i])
+                    k_sum(k - 1, i + 1, current_target - nums[i])
+                    demp.pop()
+                return 
             l, r = start, len(nums) - 1
             while l < r:
-                if nums[l] + nums[r] < currentTarget:
+                if nums[l] + nums[r] < current_target:
                     l += 1
-                elif nums[l] + nums[r] > currentTarget:
+                elif nums[l] + nums[r] > current_target:
                     r -= 1
                 else:
-                    ans.append(quad + [nums[l], nums[r]])
+                    ans.append(demp + [nums[l], nums[r]])
                     l += 1
-                    while l < r and nums[l] == nums[l -1]:
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
-        kSum(4, 0, target)
+        k_sum(4, 0, target)
         return ans
