70322e6a38f737f49717e38f4dae72168d1d47f2
Shubham Dohare <sdohare11@gmail.com>
2024-03-28 21:45:17 +0530

    Time: 89 ms (59.51%), Space: 20.3 MB (42.20%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,12 @@
 class Solution:
     def groupAnagrams(self, arr: List[str]) -> List[List[str]]:
         ans = []
-        indexMap = {}
-        for item in arr:
-            sor = ''.join(sorted(item))
-            if indexMap.get(sor) is not None:
-                ans[indexMap[sor]].append(item)
-            else:
-                ans.append([item])
-                indexMap[sor] = len(ans) - 1
+        arr = [(item, "".join(sorted(item))) for item in arr]
+        cache = {}
+        for item, sorted_item in arr:
+            if cache.get(sorted_item) is None:
+                cache[sorted_item] = []
+            cache.get(sorted_item).append(item)
+        for _, values in cache.items():
+            ans.append(values)
         return ans
-                
